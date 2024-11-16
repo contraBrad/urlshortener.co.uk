ef86d071d22c1317e95fdd39dbd2a6c4614ce276
contraBrad <bradclarke14@gmail.com>
2024-11-16 08:14:15 +0000

    Sort out web route

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,6 @@ use App\Livewire\UrlShortener;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UrlShortenerController;
 
-Route::get('/', function () 
-{
-   return view('urlshortener.index'); 
-})->name('urlshortener.index');
+Route::get('/', [UrlShortenerController::class, 'index'])->name('urlshortener.index');
 
 Route::get('/l/{shortCode}', [UrlShortenerController::class, 'redirect'])->name('urlshortener.short');
